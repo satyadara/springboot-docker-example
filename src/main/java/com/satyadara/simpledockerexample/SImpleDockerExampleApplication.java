7432fc99778dad27071b091e95fb78a779dbe942
Satya Syahputra <satya.syahputra@mokapos.com>
2020-02-18 15:36:16 +0700

    Init Project

---
+++ src/main/java/com/satyadara/simpledockerexample/SImpleDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.satyadara.simpledockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SImpleDockerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SImpleDockerExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/satyadara/simpledockerexample/SImpleDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyadara.simpledockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SImpleDockerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
